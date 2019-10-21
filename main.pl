4ccd27fc7f81b9bc8854d84700e11ff78f9ee755
Natán Fernandez de Castro <48105518+natanfdecastro@users.noreply.github.com>
2019-10-21 15:07:55 -0600

    [+] Dijkstra funcional

--- main.pl
+++ main.pl
@@ -16,6 +16,9 @@
 % [ Hechos ]
 % --------------------------------------
 
+:-dynamic
+	rpath/2.      % A reversed path
+
 edge(a,b,10,0.5).
 edge(b,c,3,0.1).
 edge(b,e,5,0.2).
@@ -30,34 +33,35 @@ edge(h,f,1,0.05).
 edge(h,i,6,0.3).
 
 
-path(From,To,Dist) :- edge(To,From,Dist);
-						edge(From,To,Dist).
-
-go(From, To) :-
-	traverse(From),    % Find all distances
-	rpath([To|RPath], Dist)->         % If the target was reached
-		reverse([To|RPath], Path),      % Report the path and distance
-		Distance is round(Dist),
-		writef('Shortest path is %w with distance %w = %w\n',
-			[Path, Dist, Distance]);
-	writef('There is no route from %w to %w\n', [From, To]).
-
-shorterPath([H|Path], Dist) :-                 % path < stored path? replace it
+path(From,To,Dist) :- edge(To,From,Dist).
+path(From,To,Dist) :- edge(From,To,Dist).
+ 
+shorterPath([H|Path], Dist) :-		       % path < stored path? replace it
 	rpath([H|T], D), !, Dist < D,          % match target node [H|_]
 	retract(rpath([H|_],_)),
 	writef('%w is closer than %w\n', [[H|Path], [H|T]]),
 	assert(rpath([H|Path], Dist)).
-
-shorterPath(Path, Dist) :-                     % Otherwise store a new path
+shorterPath(Path, Dist) :-		       % Otherwise store a new path
 	writef('New path:%w\n', [Path]),
 	assert(rpath(Path,Dist)).
-
-traverse(From, Path, Dist) :-               % traverse all reachable nodes
-	path(From, T, D),                   % For each neighbor
-	not(memberchk(T, Path)),            %   which is unvisited
-	shorterPath([T,From|Path], Dist+D), %   Update shortest path and distance
-	traverse(T,[From|Path],Dist+D).     %   Then traverse the neighbor
-
+ 
+traverse(From, Path, Dist) :-		    % traverse all reachable nodes
+	path(From, T, D),		    % For each neighbor
+	not(memberchk(T, Path)),	    %	which is unvisited
+	shorterPath([T,From|Path], Dist+D), %	Update shortest path and distance
+	traverse(T,[From|Path],Dist+D).	    %	Then traverse the neighbor
+ 
 traverse(From) :-
 	retractall(rpath(_,_)),           % Remove solutions
 	traverse(From,[],0).              % Traverse from origin
+traverse(_).
+ 
+go(From, To) :-
+	traverse(From),                   % Find all distances
+	rpath([To|RPath], Dist)->         % If the target was reached
+	  reverse([To|RPath], Path),      % Report the path and distance
+	  Distance is round(Dist),
+	  writef('Shortest path is %w with distance %w = %w\n',
+	       [Path, Dist, Distance]);
+	writef('There is no route from %w to %w\n', [From, To]).
+ 
