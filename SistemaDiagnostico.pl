7dd81569b82353175808a30c5c1d0d17051a55ba
KevinR98 <kevin.rojas277@outlook.es>
2019-10-28 23:44:01 -0600

    Documentacion interna.

--- SistemaDiagnostico.pl
+++ SistemaDiagnostico.pl
@@ -1,15 +1,30 @@
-%
-% 
-%
-%
+% Instituto Tecnologico de Costa Rica
+% Lenguajes de Programacion
+% Tarea Programada 3 -
+% Paradigma Logico - Prolog
+% Natan Fernandez de Castro - 2017105774
+% Kevin Rojas Salazar - 2016081582
 
+% ======================================
+% |            Ejercicio 2             |
+% ======================================
+% Se implementa un sistema experto que
+% identifica especies de insectos mediante
+% clasves dicotomicas.
+
+
+% --------------------------------------
+% [ Hechos ]
+% --------------------------------------
 :- dynamic(known/3).
 :- discontiguous menuask/3.
 :- discontiguous ask/2.
 :- use_module(library(jpl)).
 
 
-%Insectos escogidos de las claves dicotomicas.
+% --------------------------------------
+% [ Declaraciones de insectos ]
+% --------------------------------------
 insect(spheniscus_humboldti):- backbone(yes), lungs(yes), warm_blooded(yes), wings(yes), feathers(yes), webbed_feet(yes).
 insect(aratinga_solstitialis):- backbone(yes), lungs(yes), warm_blooded(yes), wings(yes), feathers(yes), webbed_feet(no), curved_beak(yes), feathered_face(yes).
 insect(geronticus_eremita):- backbone(yes), lungs(yes), warm_blooded(yes), wings(yes), feathers(yes), webbed_feet(no), curved_beak(yes), feathered_face(no).
@@ -23,6 +38,9 @@ insect(gazella_dorcas_neglecta):- backbone(yes), lungs(yes), warm_blooded(yes), 
 insect(tragelaphus_strepsiceros):- backbone(yes), lungs(yes), warm_blooded(yes), wings(no), even_toed_ungulate(yes),tusks(no),both_sexes_with_horns(no).
 
 
+% --------------------------------------
+% [ Declaraciones de atributos ]
+% --------------------------------------
 % Orden y metodos en los que seran preguntados los atributos de los insectos
 backbone(X):-ask(backbone, X).
 lungs(X):-ask(lungs, X).
@@ -41,7 +59,9 @@ skined_horns(X):-ask(skined_horns, X).
 shaggy_fur(X):-ask(shaggy_fur, X).
 
 
-
+% --------------------------------------
+% [ Declaraciones de preguntas ]
+% --------------------------------------
 %Metodos de consulta con respecto a las preguntas realizadas
 ask(Attr, Val) :- known(yes, Attr, Val), !.
 ask(Attr, Val) :- known(_, Attr, Val), !, fail.
@@ -50,12 +70,18 @@ ask(Attr, Val) :- known(yes, Attr, V), V \== Val, !, fail.
 ask(Attr, Val) :- interface(Attr,Val).
 
 
-
+% --------------------------------------
+% [ Metodos de arranque ]
+% --------------------------------------
 % Metodo que incia el sistema experto, cuando encuentra un match con X se lo devuelve a la interfaz para ser imprimido.
 start :- insect(X) , interfaceResponse(X).
 % Si el metodo start anterior regresa false realizara este metodo que indica que el insecto no fue encontrado
 start :- interfaceNotFound.
 
+
+% --------------------------------------
+% [ Interfaces graficas ]
+% --------------------------------------
 % Metodo creado para traducir los numero booleanos del JOptionPane a los caracteres 'yes' y 'no'
 numberToString(X, Y):- (X == 0) -> Y = yes; Y = no.
 
@@ -119,5 +145,8 @@ interfaceNotFound :-
 	jpl_call('javax.swing.JOptionPane', showMessageDialog, [F,'Insecto No Encontrado'], N),
 	jpl_call(F, dispose, [], _).
 
+% --------------------------------------
+% [ Comienzo del programa ]
+% --------------------------------------
 %Metodo que corre el programa apenas inicia	
 :- start.
