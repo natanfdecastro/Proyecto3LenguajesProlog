884b62111aea4e5ea7d917965ba8e861c3eea0c7
KevinR98 <kevin.rojas277@outlook.es>
2019-10-28 23:18:46 -0600

    [ADD] documentacion interna

--- SistemaDiagnostico.pl
+++ SistemaDiagnostico.pl
@@ -1,8 +1,15 @@
+%
+% 
+%
+%
+
 :- dynamic(known/3).
 :- discontiguous menuask/3.
 :- discontiguous ask/2.
 :- use_module(library(jpl)).
 
+
+%Insectos escogidos de las claves dicotomicas.
 insect(spheniscus_humboldti):- backbone(yes), lungs(yes), warm_blooded(yes), wings(yes), feathers(yes), webbed_feet(yes).
 insect(aratinga_solstitialis):- backbone(yes), lungs(yes), warm_blooded(yes), wings(yes), feathers(yes), webbed_feet(no), curved_beak(yes), feathered_face(yes).
 insect(geronticus_eremita):- backbone(yes), lungs(yes), warm_blooded(yes), wings(yes), feathers(yes), webbed_feet(no), curved_beak(yes), feathered_face(no).
@@ -15,24 +22,8 @@ insect(budorcas_taxicolor_taxicolor):- backbone(yes), lungs(yes), warm_blooded(y
 insect(gazella_dorcas_neglecta):- backbone(yes), lungs(yes), warm_blooded(yes), wings(no), even_toed_ungulate(yes),tusks(no),both_sexes_with_horns(yes), skined_horns(no), shaggy_fur(no).
 insect(tragelaphus_strepsiceros):- backbone(yes), lungs(yes), warm_blooded(yes), wings(no), even_toed_ungulate(yes),tusks(no),both_sexes_with_horns(no).
 
-%insect(pteropus_rodricensis).
-%insect(equus_zera_hartmannae).
-%insect(panthera_tigris_altaica).
-%insect(panthera_pardus_orientalis).
-%insect(uncia_uncia).
-%insect(callosciurus_prevostii).
-%insect(macropus_fulignosus).
-%insect(macroscelides_proboscideus).
-%insect(tupaia_belangeri).
-%insect(hylobates_syndactylus).
-%insect(colobus_polykomos).
-%insect(pithecia_pithecia).
-%insect(saguinus_imperator_subgrisescens).
-%insect(saguinus_oedipus).
 
-
-% Expert recogniser
-% Asks
+% Orden y metodos en los que seran preguntados los atributos de los insectos
 backbone(X):-ask(backbone, X).
 lungs(X):-ask(lungs, X).
 warm_blooded(X):-ask(warm_blooded, X).
@@ -51,35 +42,82 @@ shaggy_fur(X):-ask(shaggy_fur, X).
 
 
 
-% Remember what I've been told is correct
+%Metodos de consulta con respecto a las preguntas realizadas
 ask(Attr, Val) :- known(yes, Attr, Val), !.
-
-% % Remember what I've been told is wrong
 ask(Attr, Val) :- known(_, Attr, Val), !, fail.
-
-% Remember when I've been told an attribute has a different value
 ask(Attr, Val) :- known(yes, Attr, V), V \== Val, !, fail.
-
-% % I don't know this, better ask!
+%Metodo que consulta al usuario cada pregunta
 ask(Attr, Val) :- interface(Attr,Val).
 
 
-go :- insect(X) ,write('The insect is '), write(X), nl.
 
+% Metodo que incia el sistema experto, cuando encuentra un match con X se lo devuelve a la interfaz para ser imprimido.
+start :- insect(X) , interfaceResponse(X).
+% Si el metodo start anterior regresa false realizara este metodo que indica que el insecto no fue encontrado
+start :- interfaceNotFound.
 
+% Metodo creado para traducir los numero booleanos del JOptionPane a los caracteres 'yes' y 'no'
+numberToString(X, Y):- (X == 0) -> Y = yes; Y = no.
+
+%Interfaz que conecta con java e imprime una ventana con sus caracteristicas. Pide un input y lo agrega utilizando el metodo asserta.
 interface(Attr,Val) :-
-	atom_concat('Pregunta ',Attr, FAtom),
+	atom_concat(Attr, ': ', FAtom),
 	atom_concat(FAtom,Val,FinalAtom),
 	jpl_new('javax.swing.JFrame', ['Claves Dicotomicas'], F),
 	jpl_new('javax.swing.JLabel',['Sistema Experto de Insectos'],LBL),
 	jpl_new('javax.swing.JPanel',[],Pan),
+	jpl_new('java.awt.Color',[153, 153, 255],Color),
 	jpl_call(Pan,add,[LBL],_),
+	jpl_new('java.awt.Font', ['Verdana', 1, 48],Font),
+	jpl_call(LBL, setFont, [Font], _),
 	jpl_call(F, add, [Pan],_),
 	jpl_call(F, setLocation, [0,0], _),
-	jpl_call(F, setSize, [480,220], _),
+	jpl_call(F, setSize, [1080,720], _),
 	jpl_call(F, setVisible, [@(true)], _),
+	jpl_call(Pan, setBackground, [Color], _),
 	jpl_call(F, toFront, [], _),
-	jpl_call('javax.swing.JOptionPane', showInputDialog, [F,FinalAtom], Ans),
+	jpl_call('javax.swing.JOptionPane', showConfirmDialog, [F,FinalAtom], N), %Crea un input y guarda el valor seleccionado en N
 	jpl_call(F, dispose, [], _), 
-	write(Ans),nl,asserta(known(Ans, Attr, Val)), Ans == yes.
+	write(N),
+	numberToString(N, Ans), % Convierte el valor en los acronimos 'yes' o 'no' dependiendo de la respuesta dada.
+	write(Ans),
+	asserta(known(Ans, Attr, Val)), Ans == yes.
 
+%Interfaz que conecta con java e imprime una ventana con sus caracteristicas. Toma una variable INSECTO y la pone en un mensaje de la ventana.
+interfaceResponse(INSECTO) :-
+	jpl_new('javax.swing.JFrame', ['Claves Dicotomicas'], F),
+	jpl_new('javax.swing.JLabel',['Sistema Experto de Insectos'],LBL),
+	jpl_new('javax.swing.JPanel',[],Pan),
+	jpl_new('java.awt.Color',[153, 153, 255],Color),
+	jpl_call(Pan,add,[LBL],_),
+	jpl_new('java.awt.Font', ['Verdana', 1, 48],Font),
+	jpl_call(LBL, setFont, [Font], _),
+	jpl_call(F, add, [Pan],_),
+	jpl_call(F, setLocation, [0,0], _),
+	jpl_call(F, setSize, [1080,720], _),
+	jpl_call(F, setVisible, [@(true)], _),
+	jpl_call(Pan, setBackground, [Color], _),
+	jpl_call(F, toFront, [], _),
+	jpl_call('javax.swing.JOptionPane', showMessageDialog, [F,INSECTO], N),
+	jpl_call(F, dispose, [], _).
+
+%Interfaz que conecta con java e imprime una ventana con sus caracteristicas. Indica en una ventana de mensaje que el insecto no fue encontrado
+interfaceNotFound :-
+	jpl_new('javax.swing.JFrame', ['Claves Dicotomicas'], F),
+	jpl_new('javax.swing.JLabel',['Sistema Experto de Insectos'],LBL),
+	jpl_new('javax.swing.JPanel',[],Pan),
+	jpl_new('java.awt.Color',[153, 153, 255],Color),
+	jpl_call(Pan,add,[LBL],_),
+	jpl_new('java.awt.Font', ['Verdana', 1, 48],Font),
+	jpl_call(LBL, setFont, [Font], _),
+	jpl_call(F, add, [Pan],_),
+	jpl_call(F, setLocation, [0,0], _),
+	jpl_call(F, setSize, [1080,720], _),
+	jpl_call(F, setVisible, [@(true)], _),
+	jpl_call(Pan, setBackground, [Color], _),
+	jpl_call(F, toFront, [], _),
+	jpl_call('javax.swing.JOptionPane', showMessageDialog, [F,'Insecto No Encontrado'], N),
+	jpl_call(F, dispose, [], _).
+
+%Metodo que corre el programa apenas inicia	
+:- start.
